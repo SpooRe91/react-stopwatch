bd0ed948c26d9bb50a817547a551cfab2ef6ccca
Martin B <m.bogdanov9110@gmail.com>
2022-06-28 17:46:32 +0300

    Create Menu.js

---
+++ src/components/Menu.js
@@ -0,0 +1,183 @@
+export const Menu = () => {
+    return (<section id="gtco-menu" className="section-padding">
+        <div className="container">
+            <div className="section-content">
+                <div className="row mb-5">
+                    <div className="col-md-12">
+                        <div className="heading-section text-center">
+                            <span className="subheading">
+                                Specialties
+                            </span>
+                            <h2>
+                                Our Menu
+                            </h2>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-lg-4 menu-wrap">
+                        <div className="heading-menu">
+                            <h3 className="text-center mb-5">Breakfast</h3>
+                        </div>
+                        <div className="menus d-flex align-items-center">
+                            <div className="menu-img rounded-circle">
+                                <img className="img-fluid" src="img/breakfast-1.jpg" alt="" />
+                            </div>
+                            <div className="text-wrap">
+                                <div className="row align-items-start">
+                                    <div className="col-8">
+                                        <h4>Egg Sandwich</h4>
+                                    </div>
+                                    <div className="col-4">
+                                        <h4 className="text-muted menu-price">$30</h4>
+                                    </div>
+                                </div>
+                                <p>Meat Ball, Mie</p>
+                            </div>
+                        </div>
+                        <div className="menus d-flex align-items-center">
+                            <div className="menu-img rounded-circle">
+                                <img className="img-fluid" src="img/breakfast-1.jpg" alt="" />
+                            </div>
+                            <div className="text-wrap">
+                                <div className="row align-items-start">
+                                    <div className="col-8">
+                                        <h4>Egg Sandwich</h4>
+                                    </div>
+                                    <div className="col-4">
+                                        <h4 className="text-muted menu-price">$30</h4>
+                                    </div>
+                                </div>
+                                <p>Meat Ball, Mie</p>
+                            </div>
+                        </div>
+                        <div className="menus d-flex align-items-center">
+                            <div className="menu-img rounded-circle">
+                                <img className="img-fluid" src="img/breakfast-1.jpg" alt="" />
+                            </div>
+                            <div className="text-wrap">
+                                <div className="row align-items-start">
+                                    <div className="col-8">
+                                        <h4>Egg Sandwich</h4>
+                                    </div>
+                                    <div className="col-4">
+                                        <h4 className="text-muted menu-price">$30</h4>
+                                    </div>
+                                </div>
+                                <p>Meat Ball, Mie</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-lg-4 menu-wrap">
+                        <div className="heading-menu">
+                            <h3 className="text-center mb-5">Breakfast</h3>
+                        </div>
+                        <div className="menus d-flex align-items-center">
+                            <div className="menu-img rounded-circle">
+                                <img className="img-fluid" src="img/breakfast-1.jpg" alt="" />
+                            </div>
+                            <div className="text-wrap">
+                                <div className="row align-items-start">
+                                    <div className="col-8">
+                                        <h4>Egg Sandwich</h4>
+                                    </div>
+                                    <div className="col-4">
+                                        <h4 className="text-muted menu-price">$30</h4>
+                                    </div>
+                                </div>
+                                <p>Meat Ball, Mie</p>
+                            </div>
+                        </div>
+                        <div className="menus d-flex align-items-center">
+                            <div className="menu-img rounded-circle">
+                                <img className="img-fluid" src="img/breakfast-1.jpg" alt="" />
+                            </div>
+                            <div className="text-wrap">
+                                <div className="row align-items-start">
+                                    <div className="col-8">
+                                        <h4>Egg Sandwich</h4>
+                                    </div>
+                                    <div className="col-4">
+                                        <h4 className="text-muted menu-price">$30</h4>
+                                    </div>
+                                </div>
+                                <p>Meat Ball, Mie</p>
+                            </div>
+                        </div>
+                        <div className="menus d-flex align-items-center">
+                            <div className="menu-img rounded-circle">
+                                <img className="img-fluid" src="img/breakfast-1.jpg" alt="" />
+                            </div>
+                            <div className="text-wrap">
+                                <div className="row align-items-start">
+                                    <div className="col-8">
+                                        <h4>Egg Sandwich</h4>
+                                    </div>
+                                    <div className="col-4">
+                                        <h4 className="text-muted menu-price">$30</h4>
+                                    </div>
+                                </div>
+                                <p>Meat Ball, Mie</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-lg-4 menu-wrap">
+                        <div className="heading-menu">
+                            <h3 className="text-center mb-5">Breakfast</h3>
+                        </div>
+                        <div className="menus d-flex align-items-center">
+                            <div className="menu-img rounded-circle">
+                                <img className="img-fluid" src="img/breakfast-1.jpg" alt="" />
+                            </div>
+                            <div className="text-wrap">
+                                <div className="row align-items-start">
+                                    <div className="col-8">
+                                        <h4>Egg Sandwich</h4>
+                                    </div>
+                                    <div className="col-4">
+                                        <h4 className="text-muted menu-price">$30</h4>
+                                    </div>
+                                </div>
+                                <p>Meat Ball, Mie</p>
+                            </div>
+                        </div>
+                        <div className="menus d-flex align-items-center">
+                            <div className="menu-img rounded-circle">
+                                <img className="img-fluid" src="img/breakfast-1.jpg" alt="" />
+                            </div>
+                            <div className="text-wrap">
+                                <div className="row align-items-start">
+                                    <div className="col-8">
+                                        <h4>Egg Sandwich</h4>
+                                    </div>
+                                    <div className="col-4">
+                                        <h4 className="text-muted menu-price">$30</h4>
+                                    </div>
+                                </div>
+                                <p>Meat Ball, Mie</p>
+                            </div>
+                        </div>
+                        <div className="menus d-flex align-items-center">
+                            <div className="menu-img rounded-circle">
+                                <img className="img-fluid" src="img/breakfast-1.jpg" alt="" />
+                            </div>
+                            <div className="text-wrap">
+                                <div className="row align-items-start">
+                                    <div className="col-8">
+                                        <h4>Egg Sandwich</h4>
+                                    </div>
+                                    <div className="col-4">
+                                        <h4 className="text-muted menu-price">$30</h4>
+                                    </div>
+                                </div>
+                                <p>Meat Ball, Mie</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>);
+}
