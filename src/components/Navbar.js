5bf72f63913937b2bb35d324d41fd8aa268facbb
Martin B <m.bogdanov9110@gmail.com>
2022-06-30 17:46:30 +0300

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,69 +1,56 @@
-export const NavBar = () => {
+export const NavBar = (props) => {
+
+    let user = {
+        name: "Martin",
+        age: 30
+    }
+
 
     return (
-        < nav id="navbar-header" className="navbar navbar-expand-lg" >
-            <div className="container">
-                <a className="navbar-brand navbar-brand-center d-flex align-items-center p-0 only-mobile" href="/">
-                    <img src="img/logo.png" alt="google.com" />
-                </a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse"
-                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                    aria-label="Toggle navigation">
-                    <span className="lnr lnr-menu"></span>
-                </button>
+        <nav>
+            <ul>
+                <li>
+                    <a href="/">Начало</a>
+                </li>
+                <li>
+                    <a href="/recipe/browse">Търсене на рецепти</a>
+                </li>
 
-                <div className="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
-                    <ul className="navbar-nav d-flex justify-content-between">
-                        <li className="nav-item only-desktop">
-                            <a className="nav-link" id="side-nav-open" href="google.com">
-                                <span className="lnr lnr-menu"></span>
-                            </a>
-                        </li>
-                        <div className="d-flex flex-lg-row flex-column">
-                            <li className="nav-item active">
-                                <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="about.html">About</a>
-                            </li>
+                <li>
+                    <a href="/about">Относно</a>
+                </li>
+                <li>
+                    <a href="/contacts">Контакти</a>
+                </li>
 
-                            <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="google.com" id="navbarDropdown" role="button"
-                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    Special Dishes
-                                </a>
-                                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <a className="dropdown-item" href="special-dishes.html">Beef Steak Sauce</a>
-                                    <a className="dropdown-item" href="special-dishes.html">Salmon Zucchini</a>
-                                </div>
-                            </li>
-                        </div>
-                    </ul>
+                <li>
+                    <a href="/auth/register">Регистрация</a>
+                </li>
 
-                    <a className="navbar-brand navbar-brand-center d-flex align-items-center only-desktop" href="google.com">
-                        <img src="img/logo.png" alt="google.com" />
-                    </a>
-                    <ul className="navbar-nav d-flex justify-content-between">
-                        <div className="d-flex flex-lg-row flex-column">
-                            <li className="nav-item active">
-                                <a className="nav-link" href="menu.html">Menu</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="team.html">Team</a>
-                            </li>
+                <li>
+                    <a href="/auth/login">Вход</a>
+                </li>
 
-                            <li className="nav-item dropdown">
-                                <a className="nav-link" href="reservation.html">Reservation</a>
-                            </li>
-                        </div>
-                        <li className="nav-item">
-                            <a id="side-search-open" className="nav-link" href="public/images/chef-1">
-                                <span className="lnr lnr-magnifier"></span>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav >
+                <li>
+                    <a href="/recipe/add">Добави рецепта</a>
+                </li>
+
+                <li>
+                    <a href="/recipe/myRecipes">Моите рецепти</a>
+                </li>
+
+                <li>
+
+                    <a className="profile-name" href="/recipe/myRecipes"><strong>{user.name}</strong></a>
+
+                </li>
+
+                <li>
+                    <a href="/auth/logout" className="logout">Изход</a>
+                </li>
+
+
+            </ul>
+        </nav>
     );
 }
