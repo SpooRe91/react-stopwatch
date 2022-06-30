be2e67a36718166a856c7489c27b906da350d4d5
Martin B <m.bogdanov9110@gmail.com>
2022-06-30 21:58:25 +0300

    added 2 states, buttons for ordering and clearing, and eventHandler

--- src/components/MealItem/MealItem.js
+++ src/components/MealItem/MealItem.js
@@ -1,6 +1,24 @@
+import { useState } from "react";
+
 export const MealItem = (props) => {
 
     let { name, price } = props.children;
+    const [priceOfMeals, setPrices] = useState(0);
+    const [numberMeals, setNumberMeals] = useState(0);
+
+    let clickHandler = (e) => {
+        setPrices(n => n + price);
+        setNumberMeals(n => n + 1);
+    };
+
+    const clearHandler = (e) => {
+        setPrices(0)
+        setNumberMeals(0)
+    };
+
+    if (numberMeals === 10) {
+        clickHandler = null;
+    };
 
     return (
         <div className="container">
@@ -9,5 +27,10 @@ export const MealItem = (props) => {
                 <h4>{name}</h4>
                 <h4 className="already-reg">${price}</h4>
             </div>
+
+            <button onClick={clickHandler} name="buy">
+                Total:${priceOfMeals} Orders:{numberMeals} {numberMeals === 10 && "Max. count reached!"}
+            </button>
+            <button onClick={clearHandler} name="clear">Clear counter</button>
         </div>);
 };
