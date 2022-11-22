fde5a1504f5416e6decb0f220de35f7e8c9d51da
Martin B <m.bogdanov9110@gmail.com>
2022-11-22 23:21:00 +0200

    removed a comp

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import './css/style.css'
 import './css/ResetCSS.css'
 
 import { StopWatchComp } from "./components/Stopwatch/stopwatchComp";
-import { TimerComponent } from './components/Timer/timerComponent';
 
 function App() {
 
   return (
     <main>
       <StopWatchComp />
-      <TimerComponent />
     </main>
   );
 }
